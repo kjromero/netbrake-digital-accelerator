5034276f0356a05ef79f580af3879e5be8a57918
Kenny Romero <sastoque.kenny@gmail.com>
2025-09-04 17:00:50 -0500

    Delete 404 html bad practice

--- vite.config.ts
+++ vite.config.ts
@@ -13,7 +13,6 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: "/netbrake-digital-accelerator/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
