037de846b69bb4c262ecaa6a07c65a8765d6ee68
Kenny Romero <sastoque.kenny@gmail.com>
2025-09-03 19:33:51 -0500

    Added 404 file

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/netbrake-digital-accelerator/",
   server: {
     host: "::",
     port: 8080,
@@ -15,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: "/netbrake-digital-accelerator/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
